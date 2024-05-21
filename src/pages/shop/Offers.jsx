4f368ab78213054bf904d36e56630f4ca86bc9ed
22-vanshika <139971524+22-vanshika@users.noreply.github.com>
2024-05-21 19:16:49 +0530

    Add files via upload

---
+++ src/pages/shop/Offers.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+
+const Offers = () => {
+  return (
+    <>
+    <Navbar/>
+    <div className='min-h-screen flex flex-col text-center justify-center gap-6'>
+    <h1 className='text-3xl text-red-600'>No Offers available for now</h1>
+    <h2 className='text-5xl'>Keep Visiting for upcoming offers!</h2>
+    </div>
+    </>
+  )
+}
+
+export default Offers
