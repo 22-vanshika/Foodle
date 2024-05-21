7040bf62ef532b090c82cdb6ba02e9e11012aab3
22-vanshika <139971524+22-vanshika@users.noreply.github.com>
2024-05-21 19:41:11 +0530

    Update Offers.jsx

--- src/pages/shop/Offers.jsx
+++ src/pages/shop/Offers.jsx
@@ -5,7 +5,7 @@ const Offers = () => {
   return (
     <>
     <Navbar/>
-    <div className='min-h-screen flex flex-col text-center justify-center gap-6'>
+    <div className='min-h-screen pt-48 flex flex-col text-center gap-6'>
     <h1 className='text-3xl text-red-600'>No Offers available for now</h1>
     <h2 className='text-5xl'>Keep Visiting for upcoming offers!</h2>
     </div>
