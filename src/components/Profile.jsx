1c62b5204fac8b599c1b4e516a2186d737084e3f
22-vanshika <139971524+22-vanshika@users.noreply.github.com>
2024-10-08 16:49:30 +0530

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,53 +1,55 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../contexts/AuthProvider";
-import SampleIcon from "/SampleIcon.png"
+import SampleIcon from "/SampleIcon.png";
 import { Link } from "react-router-dom";
 
 const Profile = ({ user }) => {
-  
-  const {logOut} = useContext(AuthContext)
+  const { logOut } = useContext(AuthContext);
+
   const handleLogout = () => {
-    logOut().then(() => {
-      // Sign-out successful.s
-    }).catch((error) => {
-      // An error happened.
-    });
-  }
+    logOut()
+      .then(() => {
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // Handle any errors.
+        console.error("Logout Error:", error);
+      });
+  };
+
   return (
     <div>
-      <div className="drawer drawer-end z-50  ">
-        <input id="my-drawer-4" type="checkbox" className="drawer-toggle " />
+      <div className="drawer drawer-end z-50">
+        <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
 
-
-        <div className="drawer-content ">
+        <div className="drawer-content">
           {/* Page content here */}
           <label
-            htmlFor="my-drawer-4" 
-            className="drawer-button btn btn-ghost btn-circle avatar p-2 ml-1 "
+            htmlFor="my-drawer-4"
+            className="drawer-button btn btn-ghost btn-circle avatar p-2 ml-1"
           >
-            <div className=" border-2 border-red-600  rounded-full  p-1">
-              {
-                user.photoURL ? <img
-                alt="Pic"
-                src={user.photoURL}
-              /> : <img className="w-20   " alt="" src={SampleIcon}/>
-              }
+            <div className="border-2 border-red-600 rounded-full p-1">
+              {user?.photoURL ? (
+                <img alt="User Pic" src={user.photoURL} />
+              ) : (
+                <img className="w-20" alt="Sample Icon" src={SampleIcon} />
+              )}
             </div>
           </label>
         </div>
-        <div className="drawer-side ">
+        <div className="drawer-side">
           <label
             htmlFor="my-drawer-4"
             aria-label="close sidebar"
             className="drawer-overlay"
           ></label>
-          <ul className="menu pt-12 px-4 w-80 min-h-full bg-red-50   gap-3 text-lg text-stone-500">
+          <ul className="menu pt-12 px-4 w-80 min-h-full bg-red-50 gap-3 text-lg text-stone-500">
             {/* Sidebar content here */}
             <li className="hover:text-black">
               <a href="/update-profile">Profile</a>
             </li>
-            <li className="hover:text-black" >
-              <a href="/order" >Order</a>
+            <li className="hover:text-black">
+              <a href="/order">Order</a>
             </li>
             <li className="hover:text-black">
               <Link to="/dashboard/manage-items">Dashboard</Link>
@@ -56,9 +58,7 @@ const Profile = ({ user }) => {
               <a>Setting</a>
             </li>
             <li className="hover:text-black">
-              <a
-               onClick={handleLogout}
-              >Logout</a>
+              <a onClick={handleLogout}>Logout</a>
             </li>
           </ul>
         </div>
@@ -68,13 +68,3 @@ const Profile = ({ user }) => {
 };
 
 export default Profile;
-
-// import React from 'react'
-
-// const Profile = () => {
-//   return (
-//     <div>Profile</div>
-//   )
-// }
-
-// export default Profile
